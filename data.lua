8213ab5f3e1b655189eac9a3c7e2fab283b8dfc4
Klonan <11986037+Klonan@users.noreply.github.com>
2019-09-10 19:00:45 +0200

    fix 2 things

--- data.lua
+++ data.lua
@@ -1,7 +1,10 @@
 util = require "data/tf_util/tf_util"
 names = require("shared")
-require "data/hotkeys"
+--require "data/hotkeys"
 require "data/units/units"
 require "data/entities/entities"
 
-data.raw["character"]["character"].collision_mask = util.ground_unit_collision_mask()
+--data.raw["character"]["character"].collision_mask = util.ground_unit_collision_mask()
+local mask = data.raw["character"]["character"].collision_mask
+table.insert(mask, "not-colliding-with-itself")
+data.raw.unit[names.units.construction_drone].collision_mask = mask
