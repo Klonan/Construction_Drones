dd2f5f8c674eaac12a6f1b1e289d2e29842e7e3c
Klonan <11986037+Klonan@users.noreply.github.com>
2020-04-01 11:29:10 +0200

    Update

--- script/freeplay_interface.lua
+++ script/freeplay_interface.lua
@@ -1,35 +1,10 @@
-
-local created_items = function()
-  return
-  {
-    ["iron-plate"] = 8,
-    ["wood"] = 1,
-    ["pistol"] = 1,
-    ["firearm-magazine"] = 10,
-    ["burner-mining-drill"] = 1,
-    ["stone-furnace"] = 1,
-    [names.units.construction_drone] = 10
-  }
-end
-
-local respawn_items = function()
-  return
-  {
-    ["pistol"] = 1,
-    ["firearm-magazine"] = 10,
-    [names.units.construction_drone] = 10
-  }
-end
-
-local register_remote = function()
-  if not remote.interfaces["freeplay"] then return {} end
-  remote.call("freeplay", "set_skip_intro", true)
-  remote.call("freeplay", "set_respawn_items", respawn_items())
-  remote.call("freeplay", "set_created_items", created_items())
-end
-
 local lib = {}
 
-lib.on_init = register_remote
+lib.on_init = function()
+  if not remote.interfaces["freeplay"] then return end
+  local created_items = remote.call("freeplay", "get_created_items")
+  created_items[names.units.construction_drone] = 10
+  remote.call("freeplay", "set_created_items", created_items)
+end
 
 return lib
