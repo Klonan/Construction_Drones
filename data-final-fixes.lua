1e0c173b51a94f265760205d98a80cfe5c959412
Klonan <11986037+Klonan@users.noreply.github.com>
2020-07-05 12:33:45 +0200

    Fix gates and drones

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -19,10 +19,14 @@ end
 
 for k, rail in pairs (data.raw["curved-rail"]) do
   rail.collision_mask = rail.collision_mask or {"water-tile", "floor-layer", "item-layer"}
-  remove_from_list(rail.collision_mask, "item-layer")
-  remove_from_list(rail.collision_mask, "floor-layer")
   table.insert(rail.collision_mask, "floor-layer")
 end
+
+for k, gate in pairs (data.raw.gate) do
+  gate.opened_collision_mask = gate.opened_collision_mask or {"object-layer", "item-layer", "floor-layer", "water-tile"}
+  remove_from_list(gate.opened_collision_mask, "item-layer")
+end
+
 
 
 local name = names.units.construction_drone
