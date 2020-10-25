ad66f10cad496329d147e74c7d453336b0d71d18
Klonan <11986037+Klonan@users.noreply.github.com>
2020-10-25 12:39:22 +0100

    rail nonsense tweaking

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -10,26 +10,26 @@ local remove_from_list = function(list, value)
   end
 end
 
+local drone_collision_mask = "item-layer"
+
 for k, rail in pairs (data.raw["straight-rail"]) do
-  rail.collision_mask = rail.collision_mask or {"water-tile", "floor-layer", "item-layer"}
-  remove_from_list(rail.collision_mask, "item-layer")
-  remove_from_list(rail.collision_mask, "floor-layer")
-  table.insert(rail.collision_mask, "floor-layer")
+  rail.collision_mask = rail.collision_mask or {"water-tile", "floor-layer", "item-layer", "rail-layer"}
+  remove_from_list(rail.collision_mask, drone_collision_mask)
+  remove_from_list(rail.collision_mask, "rail-layer")
+  table.insert(rail.collision_mask, "rail-layer")
 end
 
 for k, rail in pairs (data.raw["curved-rail"]) do
-  rail.collision_mask = rail.collision_mask or {"water-tile", "floor-layer", "item-layer"}
-  remove_from_list(rail.collision_mask, "item-layer")
-  remove_from_list(rail.collision_mask, "floor-layer")
-  table.insert(rail.collision_mask, "floor-layer")
+  rail.collision_mask = rail.collision_mask or {"water-tile", "floor-layer", "item-layer", "rail-layer"}
+  remove_from_list(rail.collision_mask, drone_collision_mask)
+  remove_from_list(rail.collision_mask, "rail-layer")
+  table.insert(rail.collision_mask, "rail-layer")
 end
 
 for k, gate in pairs (data.raw.gate) do
   gate.opened_collision_mask = gate.opened_collision_mask or {"object-layer", "item-layer", "floor-layer", "water-tile"}
-  remove_from_list(gate.opened_collision_mask, "item-layer")
+  remove_from_list(gate.opened_collision_mask, drone_collision_mask)
 end
 
-
-
 local name = names.units.construction_drone
-data.raw.unit[name].collision_mask = {"not-colliding-with-itself", "doodad-layer", "item-layer", "consider-tile-transitions"}
+data.raw.unit[name].collision_mask = {"not-colliding-with-itself", "doodad-layer", drone_collision_mask, "consider-tile-transitions"}
